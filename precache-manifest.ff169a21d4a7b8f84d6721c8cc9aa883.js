94ba43f1ae49f62251f8729b0abe86106fd9dc68
YishengSun <yisheng4@illinois.edu>
2020-01-18 15:30:08 -0600

    Updates

---
+++ precache-manifest.ff169a21d4a7b8f84d6721c8cc9aa883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d240b4bd7b879935536e04b6ce12ae1a",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "e5f8cbbb99142c1ae1eb",
+    "url": "/Todo-List/static/css/main.a337d07f.chunk.css"
+  },
+  {
+    "revision": "aa151a6e20e5000e8584",
+    "url": "/Todo-List/static/js/2.f96a9bc8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Todo-List/static/js/2.f96a9bc8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5f8cbbb99142c1ae1eb",
+    "url": "/Todo-List/static/js/main.4595c587.chunk.js"
+  },
+  {
+    "revision": "0a7cb633adb66e080bab",
+    "url": "/Todo-List/static/js/runtime-main.3702422c.js"
+  }
+]);
